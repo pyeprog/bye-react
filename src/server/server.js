cf8c6ea83d2ed46633340aa8811991327512c484
Pye <pyeprog@foxmail.com>
2018-03-07 14:13:40 +0800

    change port

--- src/server/server.js
+++ src/server/server.js
@@ -182,6 +182,6 @@ app.post('/data/rite/:type', (req, res) => {
   }
 });
 
-app.listen(80, () => {
-  console.log('The server is running on port 80');
+app.listen(3000, () => {
+  console.log('The server is running on port 3000');
 });
